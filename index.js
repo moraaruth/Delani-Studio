19c0b8584452639ca8f1ab4b340e92ddbb72fdaf
Ruth Moraa <moraar37@gmail.com>
2022-03-14 15:06:27 +0300

    changing hide and show timing

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
     $("#design_image").click(function(){
-      $("#design_image").slideDown('1500').hide('1000');
+      $("#design_image").slideDown('1800').hide('500');
       $("#design").show('1500');
     });
     $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
+      $("#design").slideUp('1200');
+      $("#design-image").slideDown('1200');
     });
   });
 
   $(document).ready(function(){
     $("#development_image").click(function(){
-      $("#development_image").slideDown('1500').hide('1000');
+      $("#development_image").slideDown('1800').hide('500');
       $("#development").show('1500');
     });
     $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+      $("#development").slideUp('1200');
+      $("#development-image").slideDown('1200');
     });
   });
   
 
   $(document).ready(function(){
     $("#product_image").click(function(){
-      $("#product_image").slideDown('1500').hide('1000');
+      $("#product_image").slideDown('1800').hide('500');
       $("#product").show('1500');
     });
     $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
+      $("#product").slideUp('1200');
+      $("#product-image").slideDown('1200');
     });
   });
   
